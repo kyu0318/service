a7a5295f1b3797b60c67536e87d8f1e981cb30f7
Kim Kyu Hyun <kyu15.502971@gmail.com>
2026-08-31 15:33:59 +0900

    disastor 폴더 삭제

--- disastor-point-project-main/src/main/java/com/example/demo/repository/EarthquakeShelterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.EarthquakeShelter;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EarthquakeShelterRepository extends JpaRepository<EarthquakeShelter, Long> {
-}
